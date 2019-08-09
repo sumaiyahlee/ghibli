74b2cceb713c665192667614d28f8a6d9dcc076c
sumaiyahlee <sumaiyahlee.sl@gmail.com>
2019-08-08 17:22:06 -0700

    Basic Search

--- main.py
+++ main.py
@@ -1,34 +1,30 @@
-import webapp2
-import jinja2
 import os
 import json
+import webapp2
+import jinja2
 from urllib import urlencode
 from google.appengine.api import urlfetch
 
-the_jinja_env = jinja2.Environment(
+jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
 class MainPageHandler(webapp2.RequestHandler):
     def get(self):
-        form_template = the_jinja_env.get_template('templates/main.html')
-        self.response.write(form_template.render())
+        template = jinja_env.get_template('templates/main.html')
+        self.response.write(template.render())
 
-# class RecipeDisplayHandler(webapp2.RequestHandler):
-#     def post(self):
-#         query = self.request.get('query')
-#         base_url = 'http://www.recipepuppy.com/api/?'
-#         params = { 'q': query}
-#         response = urlfetch.fetch(base_url + urlencode(params)).content
-#         results = json.loads(response)
-
-#         result_template = the_jinja_env.get_template('templates/recipe.html')
-#         self.response.write(result_template.render({
-#             'results': results
-#         }))
-
+class SearchHandler(webapp2.RequestHandler):
+    def post(self):
+        filter = self.request.get('filter')
+        base_url = 'https://ghibliapi.herokuapp.com/{}'.format(filter) # JUST FOR NOW
+        response = json.loads(urlfetch.fetch(base_url).content)
+        template = jinja_env.get_template('templates/results.html')
+        self.response.write(template.render({ 'response': response}))
+        
 
 app = webapp2.WSGIApplication([
-    ('/', MainPageHandler)
+    ('/', MainPageHandler),
+    ('/search', SearchHandler),
 ], debug=True)
